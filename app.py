f68b50faf63083186c5e825e0960ed14459a4030
David <david.diaz.tunjano@gmail.com>
2023-03-28 18:57:57 -0300

    feat: add endpoint

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+stores = [
+    {
+        "name": "TheStore",
+        "items": [
+            {"name": "Chair", "price": 14.5},
+            {"name": "Table", "price": 30},
+            {"name": "Ladder", "price": 5},
+        ],
+    }
+]
+
+
+@app.route("/store", methods=["GET"])
+def get():
+    return {"stores": stores}
