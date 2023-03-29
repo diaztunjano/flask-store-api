cfbceec4d8aef600ef6805c653d7d84c8f6375df
David <david.diaz.tunjano@gmail.com>
2023-03-29 15:10:17 -0300

    fix: separate items from stores

--- app.py
+++ app.py
@@ -1,50 +1,45 @@
+import uuid
 from flask import Flask, request
+from db import items, stores
 
 app = Flask(__name__)
-
-stores = [
-    {
-        "name": "TheStore",
-        "items": [
-            {"name": "Chair", "price": 14.5},
-            {"name": "Table", "price": 30},
-            {"name": "Ladder", "price": 5},
-        ],
-    }
-]
 
 
 @app.route("/stores", methods=["GET"])
 def get_stores():
-    return {"stores": stores}
+    return {"stores": list(stores.values())}
 
 
 @app.route("/store", methods=["GET", "POST"])
 def create_store():
-    request_data = request.get_json()
-    new_store = {"name": request_data["name"], "items": []}
-    stores.append(new_store)
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    new_store = {**store_data, "id": store_id}
+    stores[store_id] = new_store
 
     return new_store, 201
 
 
-@app.route("/store/<string:name>/item", methods=["GET", "POST"])
-def create_item(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store["name"] == name:
-            new_item = {"name": request_data["name"], "price": request_data["price"]}
-            store["items"].append(new_item)
-            return new_item, 201
-    return {"message": "Store not found"}, 404
+@app.route("/item", methods=["GET", "POST"])
+def create_item():
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
+        return {"message": "Store not found"}, 404
+
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+
+    items[item_id] = item
+
+    return item, 201
 
 
-@app.route("/stores/<string:name>", methods=["GET"])
-def get_specific_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return {"store": store}
-    return {"message": "Store not found"}, 404
+@app.route("/stores/<string:store_id>", methods=["GET"])
+def get_specific_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        return {"message": "Store not found"}, 404
 
 
 @app.route("/stores/<string:name>/items", methods=["GET"])
---
+++ db.py
@@ -0,0 +1,12 @@
+items = {
+    1: {
+        "name": "Chair",
+        "price": 14.5,
+    },
+    2: {
+        "name": "Table",
+        "price": 55,
+    },
+}
+
+stores = {}
