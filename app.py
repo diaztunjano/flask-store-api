85d20575d4d47becd84259dc99237b7612455230
David <david.diaz.tunjano@gmail.com>
2023-03-30 09:56:09 -0300

    fix: add resources folder and store.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from db import items, stores
 app = Flask(__name__)
 
 
-@app.route("/store", methods=["GET", "POST"])
+@app.route("/stores", methods=["GET", "POST"])
 def create_store():
     store_data = request.get_json()
 
@@ -24,7 +24,7 @@ def create_store():
     return new_store, 201
 
 
-@app.route("/stores", methods=["GET"])
+@app.route("/store", methods=["GET"])
 def get_stores():
     return {"stores": list(stores.values())}
 
@@ -32,12 +32,21 @@ def get_stores():
 @app.route("/stores/<string:store_id>", methods=["GET"])
 def get_store(store_id):
     try:
-        return stores[int(store_id)]
+        return stores[store_id]
     except KeyError:
         return abort(404, message="Store not found")
 
 
-@app.route("/item", methods=["GET", "POST"])
+@app.route("/stores/<string:store_id>", methods=["DELETE"])
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return "Store succesfully deleted", 200
+    except KeyError:
+        return abort(404, message="Store not found")
+
+
+@app.route("/items", methods=["GET", "POST"])
 def create_item():
     item_data = request.get_json()
 
@@ -68,7 +77,7 @@ def create_item():
     return item, 201
 
 
-@app.route("/items", methods=["GET"])
+@app.route("/item", methods=["GET"])
 def get_items():
     return {"items": list(items.values())}
 
@@ -83,8 +92,10 @@ def get_item(item_id):
 
 @app.route("/items/<string:item_id>", methods=["PUT"])
 def modify_item(item_id):
+    item_data = request.get_json()
+    if "name" not in item_data or "price" not in item_data:
+        return abort(400, message="Missing required fields ('name', 'price')")
     try:
-        item_data = request.get_json()
         items[item_id].update(item_data)
         return items[item_id]
     except KeyError:
